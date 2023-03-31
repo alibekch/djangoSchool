0e125fc9b77f91ca1a96bd1a84f1f1b2dfb3cec6
rus-m <rustam.mirsakiyev@gmail.com>
2023-03-31 12:35:01 +0600

    Alike, salem #2

--- employee/models.py
+++ employee/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from main.models import Subject, School
+
+
 # Create your models here.
 class Teacher(models.Model):
     first_name = models.CharField()
@@ -10,6 +12,7 @@ class Teacher(models.Model):
     phone_number = models.CharField()
     school = models.ForeignKey(School, on_delete=models.CASCADE)
 
+
 class Director(models.Model):
     first_name = models.CharField()
     last_name = models.CharField()
@@ -17,6 +20,7 @@ class Director(models.Model):
     email = models.CharField()
     phone_number = models.CharField()
     school = models.OneToOneField(School, on_delete=models.CASCADE)
+
 
 class Deputy(models.Model):
     first_name = models.CharField()
